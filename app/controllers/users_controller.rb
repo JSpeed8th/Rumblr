9f43dc71a4350a3938915b3daf73828973d247f8
Jordan <jspeed8th@gmail.com>
2018-11-17 22:46:42 -0500

    Created route for user edit view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,13 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!
+
+  def edit
+    # @user = User.find(params[:id])
+    @user = User.find(current_user.id)
+  end
+
+  private
+
+  # params.require(:user).permit(:email, )
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
 
   devise_for :users
 
+  get "/users/:id/edit" => "users#edit"
+
   resources :users
   resources :blogs do
     resources :comments
